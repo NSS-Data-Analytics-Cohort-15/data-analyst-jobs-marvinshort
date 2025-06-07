bc7ce9cb94dbc9a81ed555d4ae907ce7e20c7b6b
marvinshort <marvin.short@gmail.com>
2025-06-07 08:10:22 -0500

    Data Analyst Jobs FINAL (06-07-2025)

--- scripts/Data Analyst Jobs Script.sql
+++ scripts/Data Analyst Jobs Script.sql
@@ -109,23 +109,74 @@
 -- ANSWER = There are 230 unique job titles for California companies.
 
 
--- 9a.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+-- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
-SELECT DISTINCT(company), AVG(star_rating) as avg_star_rating, review_count
-FROM data_analyst_jobs
-WHERE review_count >5000
-GROUP BY company, review_count
-
-
--- 9b. How many companies are there with more that 5000 reviews across all locations?
-
-SELECT DISTINCT(company), AVG(star_rating) as avg_star_rating, review_count
-FROM data_analyst_jobs
-WHERE review_count >5000
-GROUP BY company, review_count
-
+-- SELECT DISTINCT(company), ROUND(AVG(star_rating),2) as avg_star_rating, review_count
+-- FROM data_analyst_jobs
+-- WHERE review_count >5000
+-- GROUP BY company, review_count
 
 -- ANSWER = There are 46 companies with more than 5000 reviews across all locations.
 
 
+
+-- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+
+-- SELECT DISTINCT(company), AVG(star_rating) as avg_star_rating, review_count
+-- FROM data_analyst_jobs
+-- WHERE review_count >5000
+-- GROUP BY company, review_count
+-- ORDER BY avg_star_rating DESC
+
+
+-- ANSWER = American Express has the highest average star rating. The rating is 4.199999809
+
+
+
+--11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+
+-- SELECT title
+-- FROM data_analyst_jobs
+-- WHERE title LIKE '%Analyst%'
+
+
+-- ANSWER = There are 1636 jobs with Analyst in their description
+
+
+
+--12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+-- SELECT title
+-- FROM data_analyst_jobs
+-- WHERE title NOT ILIKE '%Analyst%'
+-- AND title NOT ILIKE '%Analytics%'
+
+
+
+-- ANSWER = There are 4 job titles  that do not contain the words Analyst or Analytics. They all have the word Tableau in common.
+
+
+
+-- BONUS
+-- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+--Disregard any postings where the domain is NULL. 
+-- Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+-- Which three industries are in the top 3 on this list? How many jobs have been listed for more than 3 weeks for each of the top  3?
+
+
+-- SELECT DISTINCT(domain), count(title) as number_of_jobs_with_sql
+-- FROM data_analyst_jobs
+-- WHERE skill ILIKE '%SQL%'
+-- AND days_since_posting > 21
+-- AND domain is not null
+-- GROUP BY domain, title
+-- ORDER BY number_of_jobs_with_sql DESC
+-- limit 3
+
+-- ANSWER = Consulting and Business Services, Internet and Software, and Insurance are the Top 3 Industries with SQL as a skill requirement.
+
+-- ANSWER
+-- Consulting and Business Services = 19 Jobs
+-- Internet and Software = 14 Jobs
+-- Insurance = 10 Jobs
 
